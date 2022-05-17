e89a434b248e7b6158bd0c62594c98ff8daa20b0
samu.karli <samu.karli@limaeurope.hu>
2022-05-17 15:33:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import tkinter # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    top = tkinter.Tk()
+    top.mainloop()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
